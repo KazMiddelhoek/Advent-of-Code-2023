83cea6292d9c1d9ab19809e882902620e2870df5
Kaz <kaz.middelhoek@ns.nl>
2023-12-05 16:03:32 +0100

    [skip ci] already better

--- Day 05/day05.py
+++ Day 05/day05.py
@@ -1,27 +1,27 @@
-# with open("Day 05/input.txt") as file:
-#     lines = file.read().splitlines()
+with open("Day 05/input.txt") as file:
+    lines = file.read().splitlines()
 
-# numbers_to_convert = [int(number) for number in lines[0].split(": ")[1].split()]
+numbers_to_convert = [int(number) for number in lines[0].split(": ")[1].split()]
 
-# new_numbers_to_convert = []
-# converted_numbers = []
-# for conversion_line in lines[2:]:
-#     if conversion_line == "":
-#         new_numbers_to_convert += [number for number in numbers_to_convert if number not in converted_numbers]
-#         numbers_to_convert = new_numbers_to_convert
-#         new_numbers_to_convert = []
-#         converted_numbers = []
-#         continue
+new_numbers_to_convert = []
+converted_numbers = []
+for conversion_line in lines[2:]:
+    if conversion_line == "":
+        new_numbers_to_convert += [number for number in numbers_to_convert if number not in converted_numbers]
+        numbers_to_convert = new_numbers_to_convert
+        new_numbers_to_convert = []
+        converted_numbers = []
+        continue
 
-#     if not conversion_line[0].isdigit():
-#         continue
+    if not conversion_line[0].isdigit():
+        continue
         
-#     destination_range_start, source_range_start, range_length = [int(n) for n in conversion_line.split()]
-#     for number in numbers_to_convert:
-#         if source_range_start <= number <= source_range_start + range_length:
-#             new_numbers_to_convert += [destination_range_start + number - source_range_start]
-#             converted_numbers += [number]
-# print(min(numbers_to_convert))
+    destination_range_start, source_range_start, range_length = [int(n) for n in conversion_line.split()]
+    for number in numbers_to_convert:
+        if source_range_start <= number <= source_range_start + range_length:
+            new_numbers_to_convert += [destination_range_start + number - source_range_start]
+            converted_numbers += [number]
+print(min(numbers_to_convert))
 
 
 # part two
@@ -46,37 +46,31 @@ for conversion_line in lines[2:]:
         
     destination_range_start, source_range_start, range_length_line = [int(n) for n in conversion_line.split()]
     for to_convert_range_start, to_convert_range_length_number in number_ranges_to_convert.copy():
-        if (source_range_start <= to_convert_range_start <= source_range_start + range_length_line and
-                source_range_start <= to_convert_range_start + to_convert_range_length_number <= source_range_start + range_length_line
-            ): 
-            #   --------
-            #     ----
-            overlapping_range_start,overlapping_range_length = max(source_range_start, to_convert_range_start), min(source_range_start+range_length_line, to_convert_range_start + to_convert_range_length_number) - max(source_range_start, to_convert_range_start)
-            new_number_ranges_to_convert += [( overlapping_range_start - (source_range_start - destination_range_start), overlapping_range_length)]
-            converted_numbers += [(overlapping_range_start,overlapping_range_length)]
+        number_ranges_to_convert.remove((to_convert_range_start, to_convert_range_length_number))
 
-        elif (to_convert_range_start < source_range_start and to_convert_range_start+to_convert_range_length_number >= source_range_start):
+        if (to_convert_range_start + to_convert_range_length_number < source_range_start or 
+            to_convert_range_start > source_range_start + range_length_line): # no overlap
+            number_ranges_to_convert += [(to_convert_range_start, to_convert_range_length_number)]
+            continue
+
+        overlapping_range_start = max(source_range_start, to_convert_range_start)
+        overlapping_range_length = min(source_range_start+range_length_line, to_convert_range_start + to_convert_range_length_number) - overlapping_range_start
+        new_number_ranges_to_convert += [( overlapping_range_start - (source_range_start - destination_range_start), overlapping_range_length)]
+        converted_numbers += [(overlapping_range_start,overlapping_range_length)]
+
+        if (to_convert_range_start < source_range_start and to_convert_range_start+to_convert_range_length_number >= source_range_start):
             #      ----------
             # ---------
-            overlapping_range_start,overlapping_range_length = max(source_range_start, to_convert_range_start), min(source_range_start+range_length_line, to_convert_range_start + to_convert_range_length_number) - max(source_range_start, to_convert_range_start)
-            new_number_ranges_to_convert += [( overlapping_range_start - (source_range_start - destination_range_start), overlapping_range_length)]
-            converted_numbers += [(overlapping_range_start,overlapping_range_length)]
             number_ranges_to_convert += [(to_convert_range_start, overlapping_range_start-to_convert_range_start)]
-
-
 
         elif (source_range_start <= to_convert_range_start <= source_range_start + range_length_line and to_convert_range_start+to_convert_range_length_number>source_range_start+range_length_line):
             #     -----------
             #          ----------
-            overlapping_range_start,overlapping_range_length = max(source_range_start, to_convert_range_start), min(source_range_start+range_length_line, to_convert_range_start + to_convert_range_length_number) - max(source_range_start, to_convert_range_start)
-            new_number_ranges_to_convert += [( overlapping_range_start - (source_range_start - destination_range_start), overlapping_range_length)]
-            converted_numbers += [(overlapping_range_start,overlapping_range_length)]
-            number_ranges_to_convert += [(overlapping_range_start+overlapping_range_length, 
-                                          to_convert_range_start+to_convert_range_length_number - (source_range_start+range_length_line)
-                                        )]
-        else:
-            number_ranges_to_convert += [(to_convert_range_start, to_convert_range_length_number)]
-        number_ranges_to_convert.remove((to_convert_range_start, to_convert_range_length_number))
+            number_ranges_to_convert += [(
+                overlapping_range_start+overlapping_range_length, 
+                to_convert_range_start+to_convert_range_length_number - (source_range_start+range_length_line)
+            )]
+
 
 
 print(min(number_ranges_to_convert, key=lambda x: x[0])[0])
